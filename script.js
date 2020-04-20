ebea3ae128b885222100707c608b3c17e4726697
guzelku <62844142+guzelku@users.noreply.github.com>
2020-04-21 01:41:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+﻿let DomElement=function () {
+    
+
+    selector='#block';
+
+  height='200px'; 
+
+width='200px';
+
+ bg= '#89EBF2'; 
+
+  fontSize='16pt';
+
+};
+
+DomElement.prototype.ride=function () {
+    if( selector ==='.block'){
+        const elem = document.createElement('div');
+        elem.classList.add(selector);
+        elem.style.height=height;
+        elem.style.width=width;
+        elem.style.backgroundColor=bg;
+        elem.style.fontSize=fontSize;
+        elem.innerHTML=' любой текст';
+      document.body.append(elem);
+}
+else{
+    const elem = document.createElement('p');
+    elem.id = selector;
+     elem.style.height=height;
+    elem.style.width=width;
+    elem.style.backgroundColor=bg;
+    elem.style.fontSize=fontSize;
+    elem.innerHTML=' любой текст';
+  document.body.append(elem);
+}
+};
+
+let newDiv = new DomElement();
+newDiv.ride();
+
+
